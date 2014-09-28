0f702fdce6a9084da3c1880758af1e74ae7ff9b4
OhSeunghyun <ymm26@ajou.ac.kr>
2014-09-28 13:02:53 +0900

    session

--- HumanOfAjou/apps/settings.py
+++ HumanOfAjou/apps/settings.py
@@ -5,16 +5,18 @@ Configuration for Flask app
 
 """
 
+from datetime import timedelta
+from apps.secret_keys import *
 
 class Config(object):
     # Set secret key to use session
-    SECRET_KEY = "likelion-flaskr-secret-key"
-    debug = False
+    SECRET_KEY = CSRF_SECRET_KEY
+    CSRF_SESSION_KEY = SESSION_KEY
 
 
 class Production(Config):
-    debug = True
     CSRF_ENABLED = False
     ADMIN = "ohstitch93@gmail.com"
     SQLALCHEMY_DATABASE_URI = 'mysql+gaerdbms:///flaskr?instance=osh-personal-project:osh-person'
     migration_directory = 'migrations'
+    PERMANENT_SESSION_LIFETIME = timedelta(days=1)
